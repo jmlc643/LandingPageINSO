eb3a517341886d6795e6a7a8a2b6870c2acd95d6
vicgap <vgarciap3@upao.edu.pe>
2023-10-14 05:16:08 -0500

    victor estuvo aqui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { IniciarSesionComponent } from './iniciar-sesion/iniciar-sesion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegistrarseComponent,
+    IniciarSesionComponent
   ],
   imports: [
     BrowserModule,
