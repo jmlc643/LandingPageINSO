c1654bedcb9d70f3bce7fe6d67111c9e4b7aafb7
DinoBudino <143754678+DinoBudino@users.noreply.github.com>
2023-12-02 08:30:37 -0500

    modificar environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    urlHost:'ec2-18-223-113-56.us-east-2.compute.amazonaws.com/'
+    urlHost:'http://ec2-18-223-113-56.us-east-2.compute.amazonaws.com/'
 
 };
