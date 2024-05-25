4317f3f8a16befa28335885f9a94a24b1cca0b4a
IamSebx <brunose0806@gmail.com>
2024-05-24 21:01:41 -0500

    Reapply "123"
    This reverts commit 953f4adfc18a1c93111a1275df65eb2c21b84ede.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LearnSync'`, () => {
+  it(`should have as title 'EDUCOM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LearnSync');
+    expect(app.title).toEqual('EDUCOM');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LearnSync app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('EDUCOM app is running!');
   });
 });
